d7e85a7d6961ca8a59ea5943f6c92e1fdb42bbdb
amirbarati0102SABZ <amirbarati0102@gmail.com>
2026-05-28 07:35:59 +0330

    final fix

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 # دیتابیس Render PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://piano_user:H2nWMsj9VcObtKji8igAQ7DJLJBzlAwM@dpg-d8blnu8jo6nc73e633dg-a.virginia-postgres.render.com/piano_coffee'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://piano_user:H2nWMsj9VcObtKji8igAQ7DJLJBzlAwM@dpg-d8blnu8jo6nc73e633dg-a.virginia-postgres.render.com/piano_coffee"
+)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -23,21 +27,23 @@ class Drink(db.Model):
 
     category = db.Column(db.String(100))
 
+
 # صفحه اصلی
 @app.route('/')
-
 def home():
 
     drinks = Drink.query.all()
 
     return render_template("index.html", drinks=drinks)
 
+
 # ساخت جدول
 with app.app_context():
 
     db.create_all()
 
+
 # اجرای پروژه
 if __name__ == "__main__":
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
